b129eb5f35f2797e515f4c90964a4c44fc649c3b
Nirajan Shrestha <nirajanshrestha@Nirajans-MacBook-Pro.local>
2023-09-30 18:01:19 +0545

    first commit

---
+++ InstaProfileSwiftUI/InstaProfileSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaProfileSwiftUIApp.swift
+//  InstaProfileSwiftUI
+//
+//  Created by Nirajan Shrestha on 30/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstaProfileSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
